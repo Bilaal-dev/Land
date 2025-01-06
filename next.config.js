04ebdf8448e4d4b3b92b63c2706e65a831678ae3
Ajasa Shamsudeen <shamsudeenajasa@gmail.com>
2025-01-06 09:03:23 +0300

    next update

--- next.config.js
+++ next.config.js
@@ -2,12 +2,13 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import "./src/env.js";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
 
-/** @type {import("next").NextConfig} */
-const config = {};
+module.exports = nextConfig; // Use CommonJS syntax
 
-export default config;
 
 module.exports = {
     eslint: {
